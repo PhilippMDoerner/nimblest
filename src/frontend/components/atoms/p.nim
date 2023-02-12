fd21968df75ab332a51d68002048e53f95a35c23
Philipp Doerner <philippmdoerner@web.de>
2023-02-12 12:50:25 +0100

    Add alignment to P

--- src/frontend/components/atoms/p.nim
+++ src/frontend/components/atoms/p.nim
@@ -9,20 +9,34 @@ type FontWeight* = enum
   ultrabold
   heavy
 
+type FontAlignment* = enum
+  faLeft
+  faRight
+  faCenter
+
 viewable P:
   text: string
+  alignment: FontAlignment = FontAlignment.faLeft
   fontWeight: FontWeight = Fontweight.normal
 
 proc buildLabelRepr(state: PState): string =
   var properties = fmt"""weight="{state.fontWeight}" size="large" """
-
   result = fmt"<span {properties}>{state.text}</span>"
 
 
 method view*(state: PState): Widget =
+  let alignment: float = case state.alignment:
+    of FontAlignment.faLeft:
+      0.0
+    of FontAlignment.faRight:
+      1.0
+    of FontAlignment.faCenter:
+      0.5
+
   gui:
     Label {.expand: false.}:
       useMarkup = true
+      xAlign = alignment
       text = state.buildLabelRepr()
 
 export P
